5f5e1780b0b04841459c5fbfa8c6a00e12f5079e
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2025-07-12 16:52:07 +0900

    Implement HomeController and create index view for the Ice Breaker management system, setting the root path to the home page. The index view includes a title, an image, and links for member and duty management.

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "users#index"
+  root "home#index"
 end
